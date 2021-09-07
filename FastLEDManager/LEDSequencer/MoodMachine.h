cca3bbfb3e0177b3e8a555990fefcc8b8fb281c9
Hideaki Tai <hideaki.tai@gmail.com>
2021-09-07 12:20:36 +0900

    MoodMachine made shared behavior for all instance

--- FastLEDManager/LEDSequencer/MoodMachine.h
+++ FastLEDManager/LEDSequencer/MoodMachine.h
@@ -11,37 +11,47 @@
 namespace led {
 namespace sequencer {
 
-    class MoodMachine : public Sequencer {
+    class MoodMachineTimeline {
+        // singleton
+        MoodMachineTimeline() {}
+        MoodMachineTimeline(const MoodMachineTimeline&) = delete;
+        MoodMachineTimeline& operator=(const MoodMachineTimeline&) = delete;
+
+        size_t ref_count {0};
+
         Tween::Timeline timeline[3];
         float clr[3];  // should be float not CRGB
         float duration_ms[3] {1000, 900, 1300};
 
     public:
-        virtual ~MoodMachine() {}
+        static MoodMachineTimeline& get() {
+            static MoodMachineTimeline m;
+            return m;
+        }
 
-        MoodMachine(const String& name)
-        : Sequencer(name) {}
+        void add_count() { ++ref_count; }
+        void sub_count() { --ref_count; }
+        size_t use_count() const { return ref_count; }
+        CRGB color() const { return CRGB(clr[0], clr[1], clr[2]); }
 
-        virtual void enter() override {
+        void enter() {
             for (size_t i = 0; i < 3; ++i) {
                 clr[i] = 0;
                 restart_timeline(i);
             }
         }
 
-        virtual void update() override {
+        void update() {
             for (size_t i = 0; i < 3; ++i) {
                 if (!timeline[i].update()) {
                     restart_timeline(i);
                 }
             }
-            leds->fill_solid(CRGB(clr[0], clr[1], clr[2]));
         }
 
-        virtual void exit() override {
+        void exit() {
             for (size_t i = 0; i < 3; ++i)
                 timeline[i].clear();
-            leds->fill_solid(CRGB::Black);
         }
 
     private:
@@ -49,6 +59,36 @@ namespace sequencer {
             timeline[i].add(clr[i], true).then(random(0, 255), duration_ms[i]);
             timeline[i].mode(Tween::Mode::ONCE);
             timeline[i].start();
+        }
+    };
+
+    class MoodMachine : public Sequencer {
+    public:
+        MoodMachine(const String& name)
+        : Sequencer(name) {
+            MoodMachineTimeline::get().add_count();
+        }
+
+        virtual ~MoodMachine() {
+            MoodMachineTimeline::get().sub_count();
+        }
+
+        virtual void enter() override {
+            if (MoodMachineTimeline::get().use_count() == 1) {
+                MoodMachineTimeline::get().enter();
+            }
+        }
+
+        virtual void update() override {
+            MoodMachineTimeline::get().update();
+            leds->fill_solid(MoodMachineTimeline::get().color());
+        }
+
+        virtual void exit() override {
+            if (MoodMachineTimeline::get().use_count() == 1) {
+                MoodMachineTimeline::get().exit();
+                leds->fill_solid(CRGB::Black);
+            }
         }
     };
 
